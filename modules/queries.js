7476a48f58b233e0ec856e5ebd70c49b5e3dacdb
maxthestranger <thestrangermax@gmail.com>
2022-03-21 10:57:54 +0300

    fix: lint errors

--- modules/queries.js
+++ modules/queries.js
@@ -5,4 +5,6 @@ const bookList = JSON.parse(localStorage.getItem('book_info')) || [];
 const form = document.querySelector('.add-book-form');
 const time = document.querySelector('.time');
 
-export { navItem, container, bookDiv, bookList, form, time };
+export {
+  navItem, container, bookDiv, bookList, form, time,
+};
